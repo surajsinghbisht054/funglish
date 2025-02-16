3fcb43d12380ae1e1e91d25c45c4dee51e702999
Suraj <surajsinghbisht054@gmail.com>
2025-02-16 23:05:58 +0530

    V1

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	base: "/funglish/",
 	plugins: [vue(), Components({
 		resolvers: [
 			PrimeVueResolver()
 		]
 	})],
-});
+	build: {
+		outDir: mode === 'deploy' ? 'docs' : 'dist' // Change output to 'docs' for deploy mode
+	}
+}));
